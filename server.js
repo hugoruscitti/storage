b441c9f8f7a50b3cbd503af9fe0fb0fd88b733b3
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-01-26 12:08:55 -0300

    Creando el boceto inicial.

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+app = express();
+
+var hourMs = 1000*60*60;
+app.use(express.static(__dirname + '/../descargas', { maxAge: hourMs }));
+app.use(express.directory(__dirname + '/../descargas'));
+app.use(express.errorHandler());
+
+console.log('listen 0.0.0.0:9999');
+app.listen(9999);
