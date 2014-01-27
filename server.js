a9091b6d6c68e260d44ffb4ae90643071c7ac77e
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-01-27 12:46:15 -0300

    agregando iconos.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ app = express();
 
 var hourMs = 1000*60*60;
 app.use(express.static(__dirname + '/../descargas', { maxAge: hourMs }));
-app.use(express.directory(__dirname + '/../descargas'));
+app.use(express.directory(__dirname + '/../descargas', {icons: true}));
 app.use(express.errorHandler());
 
 console.log('listen 0.0.0.0:9999');
